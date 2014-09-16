db595da1c5b0a7bcf78dfb2643a7f419a99b0951
Andre Gebers <a.gebers@uq.edu.au>
2014-09-16 14:25:36 +1000

    merged duplicated code and also use the link provided in the relationship object (if it exists) before constructing it. fixes #123

--- angular/app/scripts/controllers/Timeline.js
+++ angular/app/scripts/controllers/Timeline.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('angularApp')
-    .controller('TimelineCtrl', function ($scope, entity, data, Uris, $state) {
+    .controller('TimelineCtrl', function ($scope, entity, data, Uris, $state, Timeline) {
 
         $scope.isShowingTimeline = false;
         $scope.entity = entity;
@@ -25,57 +25,6 @@ angular.module('angularApp')
                     'media': entity.picture.thumb
                 };
             }
-
-            var dates = [];
-            angular.forEach(data.relationships, function (relationship) {
-
-                var timelineDate = {};
-                timelineDate.startDate = relationship[Uris.QA_START_DATE].substring(0, 4);
-                if (angular.isDefined(relationship[Uris.QA_END_DATE])) {
-                    timelineDate.endDate = relationship[Uris.QA_END_DATE].substring(0, 4);
-                }
-
-                // timelineDate.headline = '<a href="#/' + relationship.subject.type + '/' + relationship.subject.encodedUri + '">' + relationship.subject.name + '</a> ' + relationship.predicate[Uris.QA_LABEL].toLowerCase() + ' <a href="#/' + relationship.object.type + '/' + relationship.object.encodedUri + '">' + relationship.object.name + '</a>';
-                timelineDate.headline = relationship.subject.name + ' ' + relationship.predicate[Uris.QA_LABEL].toLowerCase() + ' ' + relationship.object.name;
-                if (angular.isDefined(relationship[Uris.QA_TEXTUAL_NOTE])) {
-                    timelineDate.text = '<p>' + relationship[Uris.QA_TEXTUAL_NOTE] + '</p>';
-                } else {
-                    timelineDate.text = '<p></p>';
-                }
-                // Get an image
-                var url = '';
-                if (relationship.subject.uri === entity.uri) {
-                    // Use the object
-                    timelineDate.asset = {
-                        media: 'images/icon.png',
-                        thumbnail: 'images/icon.png',
-                        'caption': '<h3 style="text-transform: capitalize"><a href="#/' +
-                            relationship.object.type + '/summary?' + relationship.object.type + 'Id=' + relationship.object.encodedUri + '">' + relationship.object.name + '</a></h3>'
-                    };
-                    if (angular.isDefined(relationship.object.picture)) {
-                        url = relationship.object.picture.thumb;
-                        timelineDate.asset.thumbnail = url;
-                        timelineDate.asset.media = url;
-                    }
-                } else {
-                    // Use the subject
-                    timelineDate.asset = {
-                        media: 'images/icon.png',
-                        thumbnail: 'images/icon.png',
-                        'caption': '<h3 style="text-transform: capitalize"><a href="#/' +
-                            relationship.subject.type + '/summary?' + relationship.subject.type + 'Id=' + relationship.subject.encodedUri + '">' + relationship.subject.name + '</a></h3>'
-                    };
-                    if (angular.isDefined(relationship.subject.picture)) {
-                        url = relationship.subject.picture.thumb;
-                        timelineDate.asset.thumbnail = url;
-                        timelineDate.asset.media = url;
-                    }
-                }
-                dates.push(timelineDate);
-            });
-            $scope.dates = dates;
-
-
+            $scope.dates = Timeline.relationshipsToEvents(data.relationships, entity);
         }
-
     });
--- angular/app/scripts/services/Timeline.js
+++ angular/app/scripts/services/Timeline.js
@@ -2,56 +2,39 @@
 
 angular.module('angularApp')
     .service('Timeline', function Timeline(Uris) {
+
+      function addToTimelineDate(tld, obj) {
+        var tempType = obj.type === 'structure' ? 'project' : obj.type;
+        var link = obj.$link?obj.$link:('#/'+ tempType + '/summary?' + obj.type + 'Id=' + obj.encodedUri);
+        tld.asset = {
+          media: 'images/icon.png',
+          thumbnail: 'images/icon.png',
+          'caption': '<h3 style="text-transform: capitalize"><a href="' + link + '">' + obj.name + '</a></h3>'
+        };
+        if (angular.isDefined(obj.picture)) {
+          tld.asset.thumbnail = obj.picture.thumb;
+          tld.asset.media = obj.picture.thumb;
+        }
+        return tld;
+      }
+
         // AngularJS will instantiate a singleton by calling "new" on this function
         this.relationshipsToEvents = function (relationships, entity) {
             var dates = [];
             angular.forEach(relationships, function (relationship) {
-
                 var timelineDate = {};
                 timelineDate.startDate = relationship[Uris.QA_START_DATE].substring(0, 4);
                 if (angular.isDefined(relationship[Uris.QA_END_DATE])) {
                     timelineDate.endDate = relationship[Uris.QA_END_DATE].substring(0, 4);
                 }
-
-                // timelineDate.headline = '<a href="#/' + relationship.subject.type + '/' + relationship.subject.encodedUri + '">' + relationship.subject.name + '</a> ' + relationship.predicate[Uris.QA_LABEL].toLowerCase() + ' <a href="#/' + relationship.object.type + '/' + relationship.object.encodedUri + '">' + relationship.object.name + '</a>';
                 timelineDate.headline = relationship.subject.name + ' ' + relationship.predicate[Uris.QA_LABEL].toLowerCase() + ' ' + relationship.object.name;
                 if (angular.isDefined(relationship[Uris.QA_TEXTUAL_NOTE])) {
                     timelineDate.text = '<p>' + relationship[Uris.QA_TEXTUAL_NOTE] + '</p>';
                 } else {
                     timelineDate.text = '<p></p>';
                 }
-                // Get an image
-                var url = '';
-                if (entity && relationship.subject.uri === entity.uri) {
-                    // Use the object
-                    var tempType = relationship.object.type === 'structure' ? 'project' : relationship.object.type;
-                    timelineDate.asset = {
-                        media: 'images/icon.png',
-                        thumbnail: 'images/icon.png',
-                        'caption': '<h4 style="text-transform: capitalize"><a href="#/' +
-                            tempType + '/summary?' + relationship.object.type + 'Id=' + relationship.object.encodedUri + '">' + relationship.object.name + '</a></h4>'
-                    };
-                    if (angular.isDefined(relationship.object.picture)) {
-                        url = relationship.object.picture.thumb;
-                        timelineDate.asset.thumbnail = url;
-                        timelineDate.asset.media = url;
-                    }
-                } else {
-                    // Use the subject
-                    var tempType = relationship.subject.type === 'structure' ? 'project' : relationship.subject.type;
-                    timelineDate.asset = {
-                        media: 'images/icon.png',
-                        thumbnail: 'images/icon.png',
-                        'caption': '<h4 style="text-transform: capitalize"><a href="#/' +
-                            tempType + '/summary?' + relationship.subject.type + 'Id=' + relationship.subject.encodedUri + '">' + relationship.subject.name + '</a></h4>'
-                    };
-                    if (angular.isDefined(relationship.subject.picture)) {
-                        url = relationship.subject.picture.thumb;
-                        timelineDate.asset.thumbnail = url;
-                        timelineDate.asset.media = url;
-                    }
-                }
-                dates.push(timelineDate);
+                dates.push(addToTimelineDate(timelineDate,
+                    ((entity && (relationship.subject.uri === entity.uri))?relationship.object:relationship.subject)));
             });
             return dates;
         };
