d8b77848db40fdfc8884d087cbcbdd26910c8bc8
Craig <cmcnamara87@gmail.com>
2015-02-13 15:14:58 +1000

    test(login): add tests for login

---
+++ test/sample.environment.js
@@ -0,0 +1,9 @@
+// Common configuration files with defaults plus overrides from environment vars
+
+module.exports = {
+    // Get the username and password form the Maenad wiki
+    login: {
+        username: 'USERNAME',
+        password: 'PASSWORD'
+    }
+};
