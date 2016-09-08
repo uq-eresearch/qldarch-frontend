8735c3fede3902c671d58e5be87b8838f7ecbe2c
Friska (Dendi) Pambudi <f.pambudi@uq.edu.au>
2016-09-08 13:52:55 +1000

    fixed missing links on search result page

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -11,7 +11,23 @@ angular.module('angularApp')
         function activate() {
             $scope.query = $location.search().query;
             $http.get('/ws/search?q=' + $scope.query + '&p=0&pc=20').then(function (response) {
-                $scope.results = response.data.documents;
+              var data = response.data.documents;
+              $.each(data, function(i, item) {
+                var path;
+                if (item.type === 'person') {
+                  path = '/architect/summary?architectId=';
+                } else if (item.type === 'firm') {
+                  path = '/firm/summary?firmId=';
+                } else if (item.type === 'structure') {
+                  path = '/project/summary?structureId=';
+                } else if (item.type === 'article') {
+                  path = '/article?articleId=';
+                } else if (item.type === 'interview') {
+                  path = '/interview/';
+                }
+                data[i].link = path + btoa(item.uri);
+              });
+              $scope.results = data;
             });
         }
     });
