a1d2821627efeac58e5e8c45a1a1cc6ef5c9dd29
Friska (Dendi) Pambudi <f.pambudi@uq.edu.au>
2017-02-28 16:09:11 +1000

    added projects map for firm

--- app/scripts/states/firm.structures.js
+++ app/scripts/states/firm.structures.js
@@ -11,37 +11,48 @@ angular.module('qldarchApp').config(
               '$filter',
               '$http',
               'Uris',
-              function(firm, $filter, $http, Uris) {
+              '$q',
+              function(firm, $filter, $http, Uris, $q) {
                 /* globals _:false */
-                var typestructure = $filter('filter')(
-                    firm.relationships,
-                    function(relationship) {
-                      if (relationship.subjectype === 'structure' || relationship.objecttype === 'structure') {
-                        if (relationship.subjectype === 'structure') {
-                          relationship.structurelabel = relationship.subjectlabel;
-                        }
-                        if (relationship.objecttype === 'structure') {
-                          relationship.structurelabel = relationship.objectlabel;
-                        }
-                        return $http.get(
-                            Uris.WS_ROOT + 'archobj/' + ((relationship.subjectype === 'structure') ? relationship.subject : relationship.object))
-                            .then(function(result) {
-                              if (angular.isUndefined(relationship.media)) {
-                                relationship.media = $filter('filter')(result.data.media, function(med) {
-                                  return (med.preferred || (med.type === 'Photograph' || 'Portrait' || 'Image'));
-                                }).id;
-                              }
-                              return relationship;
-                            });
-                      }
-                    });
-                return _.uniqBy(typestructure, 'structurelabel');
+                var filteredrelationships = $filter('filter')(firm.relationships, function(relationship) {
+                  if (relationship.subjectype === 'structure' || relationship.objecttype === 'structure') {
+                    if (relationship.subjectype === 'structure') {
+                      relationship.structurelabel = relationship.subjectlabel;
+                    }
+                    if (relationship.objecttype === 'structure') {
+                      relationship.structurelabel = relationship.objectlabel;
+                    }
+                    return relationship;
+                  }
+                });
+                var promises = [];
+                var tstructures = [];
+                angular.forEach(filteredrelationships, function(structure) {
+                  var promise = $http
+                      .get(Uris.WS_ROOT + 'archobj/' + ((structure.subjectype === 'structure') ? structure.subject : structure.object)).then(
+                          function(result) {
+                            if (angular.isUndefined(structure.media)) {
+                              structure.media = $filter('filter')(result.data.media, function(med) {
+                                return (med.preferred || (med.type === 'Photograph' || 'Portrait' || 'Image'));
+                              }).id;
+                            }
+                            structure.typologies = result.data.typologies;
+                            if (angular.isDefined(result.data.latitude)) {
+                              structure.lat = result.data.latitude;
+                              structure.lon = result.data.longitude;
+                            }
+                            tstructures.push(structure);
+                            return result;
+                          });
+                  promises.push(promise);
+                });
+                return $q.all(promises).then(function() {
+                  var unique = _.uniqBy(tstructures, 'structurelabel');
+                  // console.log(unique);
+                  return unique;
+                });
               } ]
         },
-        controller : [ '$scope', 'firm', 'structures', 'LayoutHelper', function($scope, firm, structures, LayoutHelper) {
-          console.log(structures);
-          $scope.firm = firm;
-          $scope.structureRows = LayoutHelper.group(structures, 6);
-        } ]
+        controller : 'ArchitectStructuresCtrl'
       });
     });
