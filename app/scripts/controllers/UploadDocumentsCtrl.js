f9260e5150e7bb7bc7ef10d66439ea4ca73ce6d8
Friska (Dendi) Pambudi <f.pambudi@uq.edu.au>
2017-03-17 12:58:59 +1000

    added upload document(s) ability

--- app/scripts/controllers/UploadDocumentsCtrl.js
+++ app/scripts/controllers/UploadDocumentsCtrl.js
@@ -1,9 +1,51 @@
 'use strict';
 
 angular.module('qldarchApp').controller('UploadDocumentsCtrl',
-    function($scope, $upload, Uris, $http, Structure, File, $state, $stateParams, Expression) {
+    function($scope, $filter, $upload, File, $state, $stateParams, firms, architects, structures) {
+
+      function goToArticles() {
+        var params = {};
+        if ($stateParams.type === 'person') {
+          params.architectId = $stateParams.id;
+          $state.go('architect.articles', params);
+        } else if ($stateParams.type === 'firm') {
+          params.firmId = $stateParams.id;
+          $state.go('firm.articles', params);
+        } else if ($stateParams.type === 'structure') {
+          params.structureId = $stateParams.id;
+          $state.go('structure.articles', params);
+        }
+      }
 
       $scope.expressions = [];
+
+      var archobjs = [];
+
+      angular.forEach(architects, function(architect) {
+        architect.detailtype = '(Person)';
+      });
+
+      angular.forEach(firms, function(firm) {
+        firm.detailtype = '(Firm)';
+      });
+
+      angular.forEach(structures, function(structure) {
+        structure.detailtype = '(Project)';
+      });
+
+      archobjs = architects.concat(firms).concat(structures);
+
+      archobjs = $filter('orderBy')(archobjs, function(archobj) {
+        return archobj.label;
+      });
+
+      if ($stateParams.id && $stateParams.name) {
+        $scope.selectedObj = {
+          id : $stateParams.id,
+          text : $stateParams.name + ' (' + $stateParams.type.charAt(0).toUpperCase() + $stateParams.type.slice(1) + ')',
+        };
+        $scope.selectedTitle = $stateParams.name;
+      }
 
       $scope.onFileSelect = function($files) {
         // $files: an array of files selected, each file has name, size, and
@@ -12,101 +54,51 @@ angular.module('qldarchApp').controller('UploadDocumentsCtrl',
           console.log('file', file);
           // Create an expression for each file
           var expression = {};
-          expression.$article = {};
-          expression.$interview = {};
           expression.$uploadFile = file;
 
-          // Default type to articles
-          expression[Uris.RDF_TYPE] = Uris.QA_ARTICLE_TYPE;
-
           $scope.expressions.unshift(expression);
-
+          $scope.myModelObj = {
+            depicts : $stateParams.id,
+            label : $scope.selectedTitle,
+            type : 'Article'
+          };
+          expression.id = $stateParams.id;
           expression.$upload = File.upload($scope.myModelObj, file).progress(function(evt) {
             expression.$uploadFile.percent = parseInt(100.0 * evt.loaded / evt.total);
             expression.$uploadFile.isComplete = expression.$uploadFile.percent === 100;
-          }).success(function(data) {
-            // file is uploaded successfully
-            expression[Uris.QA_HAS_FILE] = data.uri;
-            expression.$file = File.setupImageUrls(data);
+          }).success(function() {
+            goToArticles();
+          }).error(function() {
+            // Something went wrong uploading the file
+            console.log('File upload failed');
+            var index = $scope.expressions.indexOf(expression);
+            $scope.expressions.splice(index, 1);
           });
         });
-
-        jQuery('html, body').animate({
-          scrollTop : jQuery('#uploads').offset().top + 'px'
-        }, 500, 'swing', function() {
-        });
       };
 
-      $scope.cancelUpload = function(expression) {
-        // Remove the expression
-        var index = $scope.expressions.indexOf(expression);
-        $scope.expressions.splice(index, 1);
-
-        // Cancel the upload
-        expression.$upload.abort();
+      $scope.cancelUpload = function() {
+        goToArticles();
       };
-
-      $scope.publish = function(expression) {
-        // Import data from type
-        if (expression[Uris.RDF_TYPE] === Uris.QA_ARTICLE_TYPE) {
-          var article = expression.$article;
-          delete expression.$article;
-          angular.extend(expression, article);
-        } else if (expression[Uris.RDF_TYPE] === Uris.QA_INTERVIEW_TYPE) {
-          var interview = expression.$interview;
-          delete expression.$article;
-          angular.extend(expression, interview);
-        }
-
-        console.log('expression', expression);
-        Expression.create(expression);
-      };
-
-      /*
-       * ===================================================== Select2 Boxes
-       * =====================================================
-       */
-      $scope.$watch('selectedProject', function(project) {
-        if (project && project.id === 'new') {
-          $state.go('structure.summary.edit', {
-            structureId : null
-          });
-        }
-      });
 
       // Setup the entity select boxes
-      $scope.structureSelect = {
-        placeholder : 'Select a Project',
+      $scope.archObjSelect = {
+        placeholder : 'Select an Architect or Firm or Project',
         dropdownAutoWidth : true,
         multiple : false,
         // minimumInputLength: 2,
         query : function(options) {
-          Structure.findByName(options.term, false).then(function(entities) {
-            var data = {
-              results : [ {
-                id : 'new',
-                text : 'Create a new Project'
-              } ]
-            };
-
-            angular.forEach(entities, function(entity) {
-              var label = entity.name + ' (' + entity.type.charAt(0).toUpperCase() + entity.type.slice(1) + ')';
-              if (entity.type === 'structure') {
-                label = entity.name + ' (Project)';
-              }
-
-              data.results.unshift({
-                id : entity.uri,
-                uri : entity.uri,
-                text : label,
-                type : entity.type,
-                name : entity.name,
-                encodedUri : entity.encodedUri,
-                picture : entity.picture
-              });
+          var data = {
+            results : []
+          };
+          angular.forEach(archobjs, function(archobj) {
+            data.results.push({
+              id : archobj.id,
+              text : archobj.label + ' ' + archobj.detailtype
             });
-            options.callback(data);
           });
+          options.callback(data);
         }
       };
+
     });
