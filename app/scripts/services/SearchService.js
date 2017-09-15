04d1cf74992606d63fcc41ad5e47a37c3953eaca
Friska (Dendi) Pambudi <f.pambudi@uq.edu.au>
2017-09-15 13:57:57 +1000

    added maximum hits for (articles) search results

--- app/scripts/services/SearchService.js
+++ app/scripts/services/SearchService.js
@@ -3,13 +3,17 @@
 
   /* @ngInject */
   function SearchService($http, Uris, WordService) {
+    /* globals _:false */
+    /* globals $:false */
     function getArticles(query) {
       var syntax = '* AND (type:article OR type:Article) AND (category:archobj OR category:media)';
-      return $http.get(Uris.WS_ROOT + 'search?q=' + query.replace(WordService.spclCharsLucene, '') + syntax + '&p=0&pc=20').then(function(response) {
-        /* globals _:false */
-        return _.map(response.data.documents, function(article) {
-          return _.assign({}, article, {
-            id : article.id
+      var url = Uris.WS_ROOT + 'search?q=' + query.replace(WordService.spclCharsLucene, '') + syntax + '&p=0';
+      return $http.get(url).then(function(resp) {
+        return $http.get(url + '&pc=' + resp.data.hits).then(function(response) {
+          return _.map(response.data.documents, function(article) {
+            return _.assign({}, article, {
+              id : article.id
+            });
           });
         });
       });
@@ -17,23 +21,25 @@
 
     function getArticlesInterviews(query) {
       var syntax = '* AND (type:article OR type:interview) AND category:archobj';
-      return $http.get(Uris.WS_ROOT + 'search?q=' + query.replace(WordService.spclCharsLucene, '') + syntax + '&p=0&pc=20').then(function(response) {
-        var data = response.data.documents;
-        var documents = [];
-        /* globals $:false */
-        $.each(data, function(i, item) {
-          if (item.type === 'article' || item.type === 'interview') {
-            var path;
-            if (item.type === 'article') {
-              path = '/article?articleId=';
-            } else if (item.type === 'interview') {
-              path = '/interview/';
+      var url = Uris.WS_ROOT + 'search?q=' + query.replace(WordService.spclCharsLucene, '') + syntax + '&p=0';
+      return $http.get(url).then(function(resp) {
+        return $http.get(url + '&pc=' + resp.data.hits).then(function(response) {
+          var data = response.data.documents;
+          var documents = [];
+          $.each(data, function(i, item) {
+            if (item.type === 'article' || item.type === 'interview') {
+              var path;
+              if (item.type === 'article') {
+                path = '/article?articleId=';
+              } else if (item.type === 'interview') {
+                path = '/interview/';
+              }
+              data[i].$link = path + item.id;
+              documents.push(data[i]);
             }
-            data[i].$link = path + item.id;
-            documents.push(data[i]);
-          }
+          });
+          return documents;
         });
-        return documents;
       });
     }
 
