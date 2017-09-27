25b7d6baa44b717a8c520b1eae6ce848b44d0a74
Friska (Dendi) Pambudi <f.pambudi@uq.edu.au>
2017-09-27 14:36:09 +1000

    modified depicts for my files

--- app/scripts/controllers/UserFilesCtrl.js
+++ app/scripts/controllers/UserFilesCtrl.js
@@ -1,8 +1,16 @@
 'use strict';
 
 angular.module('qldarchApp').controller('UserFilesCtrl', function($scope, $filter, mediaimages, LayoutHelper) {
+  var DEFAULT_IMAGE_ROW_COUNT = 5;
+  $scope.imageRowDisplayCount = DEFAULT_IMAGE_ROW_COUNT;
+
   mediaimages = $filter('orderBy')(mediaimages, function(mediaimage) {
     return mediaimage.label;
   });
+
   $scope.mediaimageRows = LayoutHelper.group(mediaimages, 6);
+
+  $scope.addMoreImageRows = function() {
+    $scope.imageRowDisplayCount += 5;
+  };
 });
